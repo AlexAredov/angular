00d8f06478156680038d8b90b3db893933378df8
Alex Aredov <59872895+AlexAredov@users.noreply.github.com>
2020-12-22 23:33:39 +0300

    вроде работает

---
+++ playlist/PlayList/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule } from '@angular/forms';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SingUpComponent,
+    SingInComponent,
+    NotFoundComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
